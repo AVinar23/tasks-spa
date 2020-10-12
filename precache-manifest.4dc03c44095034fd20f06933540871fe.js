cbef94040f4e316be94c4ee68c117a7519cc2ac0
AVinar23 <akregel23@gmail.com>
2020-10-12 20:14:13 +0300

    init

---
+++ precache-manifest.4dc03c44095034fd20f06933540871fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de24aa12aa215ca6af2b",
+    "url": "/tasks-spa/css/app.7725b591.css"
+  },
+  {
+    "revision": "32600ac85764076b1fc1952c055838ca",
+    "url": "/tasks-spa/index.html"
+  },
+  {
+    "revision": "de24aa12aa215ca6af2b",
+    "url": "/tasks-spa/js/app.f73683ad.js"
+  },
+  {
+    "revision": "49f31df73347bdff4c1c",
+    "url": "/tasks-spa/js/chunk-vendors.befba678.js"
+  },
+  {
+    "revision": "2a8aa7b08861f47ed5c3ece6d1dd398c",
+    "url": "/tasks-spa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tasks-spa/robots.txt"
+  }
+]);
